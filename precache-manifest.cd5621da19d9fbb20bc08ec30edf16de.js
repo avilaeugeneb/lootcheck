4cc578b1a7dc2e1362ad99b4d0c90d053def1e91
Eugene Avila <eugene@ctoglobal.co>
2019-04-08 18:40:46 +0800

    Updates

---
+++ precache-manifest.cd5621da19d9fbb20bc08ec30edf16de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f4f7fa745136c77ea886c6070bb7485",
+    "url": "/lootcheck/static/media/background.9f4f7fa7.jpg"
+  },
+  {
+    "revision": "1a433dc66ae4f782d181",
+    "url": "/lootcheck/static/js/runtime~main.df2bbe64.js"
+  },
+  {
+    "revision": "ea2b42ea3be38ed26600",
+    "url": "/lootcheck/static/js/main.86c0a667.chunk.js"
+  },
+  {
+    "revision": "244466fbdfa49e7c03a6",
+    "url": "/lootcheck/static/js/2.37119796.chunk.js"
+  },
+  {
+    "revision": "ea2b42ea3be38ed26600",
+    "url": "/lootcheck/static/css/main.c23cf0e0.chunk.css"
+  },
+  {
+    "revision": "2e8967348486279cc1d91e64de596e2e",
+    "url": "/lootcheck/index.html"
+  }
+];
